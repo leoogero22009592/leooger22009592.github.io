b7f0c43c1f9aecea184ed3b269bca0d306d3947b
MaximeDELIND21218703 <98822652+DELIN-Maxime@users.noreply.github.com>
2023-09-07 12:03:56 +0200

    Create index.php
    Base of the menu page

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="fr">
+<head>
+
+    <meta charset="utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Cosmeet</title>
+    <meta name="Description" content="Blog thème espace"/>
+    <meta name="author" content="Cosmeet" />
+    <meta name="Keywords" content="blog"/>
+
+</head>
+
+<body>
+
+<div id="bloc_page">
+
+    <?php
+
+    ?>
+
+</div>
+
+</body>
+
+<footer>
+
+</footer>
+
+</html>
