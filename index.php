35b8c00ade311d96746b41816a6385794a9d4ffa
ElliotLartigaud <elliotlartigaud@gmial.com>
2023-10-02 09:55:49 +0200

    noyau

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-
+define('ROOT', str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
 
 <a href="./inscription"> Inscrivez-vous</a>
 <a href="./connexion.php"> connectez-vous</a>
---
+++ php/Noyau/Exceptions/ControleurException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Exceptions;
+class ControleurException extends Exception
+{
+}
