68310139a8dd11740e4bb2c4499d6a1a356b25f8
CHOPARD-William-2225020 <123471387+WilliamCHOPARDC22003913@users.noreply.github.com>
2023-10-06 11:22:55 +0200

    Add files via upload

--- reseau social/index.php
+++ reseau social/index.php
@@ -1,19 +1,42 @@
 <?php
 
-require 'Noyau/ChargementAuto.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-function __autoload($class_name) {
-    include 'controllers/' . $class_name . '.php';
-}
+use Controleur\UsrControleur;
 
-if (isset($_GET['controller']) && isset($_GET['action'])) {
-    $controller_name = $_GET['controller'] . 'Controleur';
-    $action = $_GET['action'];
+
+// Inclure le modèle de base
+require 'Modele/UsrModels.php';
+
+// Récupérer l'URI de la demande
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Supprimer les paramètres de l'URL
+$cleanUri = strtok($requestUri, '?');
+
+// Divisez l'URI en segments en utilisant '/'
+$uriSegments = explode('/', trim($cleanUri, '/'));
+
+$controllerName = isset($uriSegments[0]) ? ucfirst($uriSegments[0]) . 'Controleur' : 'AccueilController';
+$action = isset($uriSegments[1]) ? $uriSegments[1] : 'accueil';
+
+// Inclure le fichier du contrôleur spécifié
+$controllerFile = "Controleur/{$controllerName}.php";
+
+if (file_exists($controllerFile)) {
+    require $controllerFile;
+
+    // Instancier le contrôleur
+    $controller = new $controllerName();
+
+    // Vérifier si la méthode existe dans le contrôleur
+    if (method_exists($controller, $action)) {
+        call_user_func(array($controller, $action));
+    } else {
+        include('Vues/404.php');
+    }
 } else {
-    $controller_name = 'InscriptionControleur';
-    $action = 'afficherFormulaire';
+    include('Vues/accueil.php');
 }
-
-$controller = new $controller_name();
-$controller->$action();
-?>
