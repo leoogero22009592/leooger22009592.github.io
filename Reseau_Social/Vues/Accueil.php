e7041f2bfab9085eac9ffd48b24fc676f1aafd8e
Elliot Lartigaud <elliot.lartigaud@etu.univ-amu.fr>
2023-10-22 22:24:12 +0200

    accueil

---
+++ Reseau_Social/Vues/Accueil.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="fr">
+
+<head>
+    <title>Cosmeet</title>
+</head>
+
+</body>
+    <div id="Onglets">
+        <h3><a id="connexion" href="../index.php?url=Inscription">S'INSCRIRE</a> </h3>
+    </div>  
+</body>
+
+</html>
+
+<style>
+    @import url("/Reseau_Social/CSS/Accueil.css");
+</style>
